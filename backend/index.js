bfd6c8a302baec17b2ac6e29dfd0fc060aae1826
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-05-25 10:51:35 -0400

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
+
 
 dotenv.config()
 const app = express()
@@ -11,8 +15,53 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
     console.log(err)
 })
-
+app.use(express.json())
+app.use(cookieParser())
 app.listen(3000, () => {
     console.log('serverrunning on port 3000')
 })
+
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message | 'internal server error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode: statusCode,
+        message: err.message,
+    })
+})
+
+
+
+
+
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import userRouter from './routes/user.route.js'
+// dotenv.config();
+
+// mongoose
+//     .connect(process.env.MONGO)
+//     .then(() => {
+//         console.log('Connected to MongoDB!');
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// const app = express();
+
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000!');
+// });
+
+
+
+app.use('/api/user', userRouter)
+
+
 
